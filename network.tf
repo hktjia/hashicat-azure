1d02deff7ede77dda0aa152b74ea5f5feb80cf8b
HK Tjia <28615352+hktjia@users.noreply.github.com>
2023-09-07 20:14:10 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/kkisworo-chip-bootcamp/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "rg-kkisworo-hashicat-azure"
+  resource_group_name = "kkisworo-workshop"
 }
